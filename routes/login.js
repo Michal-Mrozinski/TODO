c0966ab9421c3798b20f9f9a7736d3177a6ea58e
Michał Mroziński <m.mrozinski@onet.pl>
2021-03-08 18:40:49 +0100

    session tracking added

--- routes/login.js
+++ routes/login.js
@@ -25,13 +25,9 @@ router.post('/uzyt', (req, res) => {
         polcz.query('SELECT * FROM konta WHERE username = ? AND password = ?',
         [login, haslo], (error, results, fields) => {
             if (results.length > 0) {
-                res.json({
-                    login: login,
-                    haslo: haslo,
-                    zawartosc: results,
-                    bledy: error,
-                    pola: fields
-                })
+                req.session.loggedin = true
+                req.session.username = login
+                res.redirect('/todo')
             
             } else {
                 res.json('Nieprawidłowy login i/lub hasło')
@@ -41,5 +37,17 @@ router.post('/uzyt', (req, res) => {
         res.json('Podaj login i hasło')
     }
 })
+router.get('/todo', (req, res) => {
+    if (req.session.loggedin) {
+        res.json({
+            succ: 'logged in'
+        })
+    } else {
+        res.json({
+            fail: 'not logged in'
+        })
+    }
+})
+
 
 module.exports = router
